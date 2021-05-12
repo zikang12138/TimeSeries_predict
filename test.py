2977b342ef3ee2cb3ed9a781895c6c7c6a687c52
zikang12138 <215981062@qq.com>
2021-05-12 23:52:06 +0800

    latest

--- test.py
+++ test.py
@@ -10,19 +10,19 @@ from math import sqrt
 from pandas import read_csv
 #from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense,LSTM,Activation
+from tensorflow.keras.layers import Dense,LSTM,Activation,Dropout
 from numpy import array , hstack
 from tensorflow import keras
 import tensorflow as tf
-
+import time
 url = 'test.csv'
 dataset = pd.read_csv(url)
 x_1 = dataset['x1']
-x_2 = dataset['x2']
+y = dataset['y']
 
 
 x_1 = x_1.values
-x_2 = x_2.values
+y = y.values
 # plt.figure(figsize=(30, 6))
 # plt.plot(x_1[:10064] , label='x1')
 # plt.plot(x_2[:10064] , label='x2')
@@ -35,12 +35,12 @@ x_2 = x_2.values
 # plt.show()
 
 x_1 = x_1.reshape((len(x_1), 1))
-x_2 = x_2.reshape((len(x_2), 1))
+y = y.reshape((len(y), 1))
 
 
 print ("x_1.shape" , x_1.shape) 
-print ("x_2.shape" , x_2.shape) 
-dataset_stacked = hstack((x_1, x_2))
+print ("y.shape" , y.shape) 
+dataset_stacked = hstack((x_1, y))
 
 print ("dataset_stacked.shape" , dataset_stacked.shape) 
 def split_sequences(sequences, n_steps_in, n_steps_out):
@@ -53,7 +53,7 @@ def split_sequences(sequences, n_steps_in, n_steps_out):
 		if out_end_ix > len(sequences):
 			break
 		# gather input and output parts of the pattern
-		seq_x, seq_y = sequences[i:end_ix, :-1], sequences[end_ix-1:out_end_ix, -1]
+		seq_x, seq_y = sequences[i:end_ix, :], sequences[end_ix-1:out_end_ix, -1]
 		X.append(seq_x)
 		y.append(seq_y)
 	return array(X), array(y)
@@ -62,6 +62,27 @@ n_steps_in, n_steps_out = 60 , 30
 
 # covert into input/output
 X, y = split_sequences(dataset_stacked, n_steps_in, n_steps_out)
-
+y=np.reshape(y,(y.shape[0],y.shape[1],1))
 print ("X.shape" , X.shape) 
 print ("y.shape" , y.shape) 
+split = 6000
+train_X , train_y = X[:split, :] , y[:split, :]
+test_X , test_y = X[split:, :] , y[split:, :]
+
+n_features = train_X.shape[2]
+#optimizer learning rate
+def lstm(x_train, y_train, seq_len,label_len, ep):
+        model = Sequential()
+        model.add(LSTM(100, return_sequences=True, input_shape=(seq_len, 2)))
+        model.add(Dropout(0.2))
+        model.add(LSTM(100, return_sequences=False))
+        model.add(Dropout(0.2))
+        model.add(Dense(label_len))
+        model.add(Activation('linear'))
+        model.compile(loss='mse', optimizer='adam')
+        start = time.time()
+        model.fit(x_train, y_train, batch_size=64, epochs=ep, validation_split=0.05)
+        print('compilation time : ', time.time() - start)
+        #model.save(model_save)
+
+lstm(train_X,train_y,n_steps_in,n_steps_out,ep=30)
